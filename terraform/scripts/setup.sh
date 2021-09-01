60d7d38d57a211b4c33c293bc7786f7659cffa28
John Miller <john.miller@d2iq.com>
2021-09-01 14:51:17 -0400

    Removed duplication

--- terraform/scripts/setup.sh
+++ terraform/scripts/setup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-sudo growpart /dev/nvme0n1 2
-sudo pvresize /dev/nvme0n1p2
-sudo lvextend -L+10G /dev/VolGroup00/rootVol
-sudo lvextend -L+20G /dev/VolGroup00/varVol
-sudo lvextend -L+20G /dev/VolGroup00/homeVol
-sudo resize2fs /dev/VolGroup00/rootVol
-sudo resize2fs /dev/VolGroup00/varVol
-sudo resize2fs /dev/VolGroup00/homeVol
-
 chmod 600 ~/.ssh/id_rsa
 
 sudo yum install -y --nogpgcheck yum-utils
