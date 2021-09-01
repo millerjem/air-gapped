d931157505568e42412d43bdb5cc825860dbcc7e
John Miller <john.miller@d2iq.com>
2021-09-01 15:50:57 -0400

    Updated scripts

--- terraform/scripts/setup.sh
+++ terraform/scripts/setup.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-chmod 600 ~/.ssh/id_rsa
-
-sudo yum install -y --nogpgcheck yum-utils
-
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-    
-mkdir bootstrap
-cd bootstrap
-yumdownloader --resolve bzip2 zip unzip yum-utils docker-ce docker-ce-cli containerd.io
-
-cd ..
+# Placeholder
